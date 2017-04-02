278c6ae5bda5c67290c2d762de1348deca8e24e3
ZZY <1246133242@qq.com>
2017-04-02 09:42:02 +0800

    init DemoMasonry

---
+++ DemoMasonry/DemoMasonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoMasonry
+//
+//  Created by ZZY on 2017/4/2.
+//  Copyright © 2017年 DMY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoMasonry/DemoMasonry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoMasonry
+//
+//  Created by ZZY on 2017/4/2.
+//  Copyright © 2017年 DMY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
